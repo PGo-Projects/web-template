f62190d4cc138f7df8497b7d6019ca83fa8496a1
Patrick Chan <patrickchan1999@gmail.com>
2019-07-05 18:52:08 -0400

    Decode the base64 encoded secrets before use

--- internal/security/security.go
+++ internal/security/security.go
@@ -2,7 +2,9 @@ package security
 
 import (
 	"context"
+	"encoding/base64"
 
+	"github.com/PGo-Projects/output"
 	"github.com/PGo-Projects/web-template/internal/config"
 	"github.com/PGo-Projects/web-template/internal/securitydb"
 	"github.com/PGo-Projects/webauth"
@@ -10,22 +12,30 @@ import (
 	"github.com/spf13/viper"
 )
 
+func MustDecodeBase64(s string) []byte {
+	decoded, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		output.ErrorAndPanic(err)
+	}
+	return decoded
+}
+
 func MustSetup(mux *chi.Mux) {
 	// Setup security middleware
 	mux.Use(webauth.ExpirationMiddleware)
 
-	authenticationKey := viper.GetString(config.AuthenticationKey)
-	encryptionKey := viper.GetString(config.EncryptionKey)
-	csrfAuthenticationKey := viper.GetString(config.CSRFAuthenticationKey)
+	authenticationKey := MustDecodeBase64(viper.GetString(config.AuthenticationKey))
+	encryptionKey := MustDecodeBase64(viper.GetString(config.EncryptionKey))
+	csrfAuthenticationKey := MustDecodeBase64(viper.GetString(config.CSRFAuthenticationKey))
 
 	mongoClient := securitydb.MustMongoClient(context.TODO(), "mongodb://localhost:27017")
 	webauth.RegisterDatabase(mongoClient)
 
-	webauth.SetupSessions([]byte(authenticationKey), []byte(encryptionKey))
+	webauth.SetupSessions(authenticationKey, encryptionKey)
 	webauth.SessionOptions.Secure = config.ProdRun
 
 	allowedOrigins := viper.GetStringSlice(config.AllowedOrigins)
 	webauth.SetupCORS(mux, false, allowedOrigins)
-	webauth.SetupCSRF(mux, []byte(csrfAuthenticationKey), config.ProdRun)
+	webauth.SetupCSRF(mux, csrfAuthenticationKey, config.ProdRun)
 	webauth.RegisterEndPoints(mux)
 }
